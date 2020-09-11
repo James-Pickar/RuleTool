a7b57986fefe77e95ef22777fbb54c834bec71f7
James Pickar <jamieppickar@gmail.com>
2020-09-11 16:25:19 -0400

    weight enumation moved to other function to increase readibility

--- RuleTool.py
+++ RuleTool.py
@@ -2,6 +2,20 @@ import argparse
 from pathlib import Path
 
 
+def extract_weight(prefix: str)-> float:
+    if (prefix.find("<") > -1) and (prefix.find(">") > -1):
+        weight_start = prefix.find("<") + 1
+        weight_end = prefix.find(">")
+        weight = prefix[weight_start:weight_end]
+    else:
+        weight = 1.0
+    try:
+        weight = float(weight)
+    except ValueError:
+        weight = 1.0
+    return weight
+
+
 def read_file(file: str) -> str:
     if not (file and Path(file).is_file()):
         print("Invalid Path")
@@ -17,18 +31,7 @@ def enumerate_rules(file_text: str):
         rule_end = file_text.find(";}") + 2
         rule = file_text[rule_start:rule_end]
         rule_prefix = file_text[(rule_start - 10):rule_start]
-
-        if (rule_prefix.find("<") > -1) and (rule_prefix.find(">") > -1):
-            weight_start = rule_prefix.find("<") + 1
-            weight_end = rule_prefix.find(">")
-            weight = rule_prefix[weight_start:weight_end]
-        else:
-            weight = 1.0
-        try:
-            weight = float(weight)
-        except ValueError:
-            weight = 1.0
-
+        weight = extract_weight(rule_prefix)
         rules.append({
             "text": rule,
             "weight": weight
