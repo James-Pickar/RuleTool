cffd49fccf372fd0b7b99e88d1a4f5c61d5d27d6
James Pickar <jamieppickar@gmail.com>
2020-09-13 11:14:30 -0400

    first full draft

--- RuleTool.py
+++ RuleTool.py
@@ -2,6 +2,17 @@ import argparse
 from pathlib import Path
 
 
+class KeyValue(argparse.Action):
+    # Constructor calling
+    def __call__(self, parser, namespace, values, option_string=None):
+        setattr(namespace, self.dest, list())
+        for value in values:
+            # split it into key and value
+            key, value = value.split("=")
+            # assign into dictionary
+            getattr(namespace, self.dest).append([key, value])
+
+
 # Modular Functions
 def extract_weight(prefix: str) -> float:
     if (prefix.find("<") > -1) and (prefix.find(">") > -1):
@@ -9,11 +20,11 @@ def extract_weight(prefix: str) -> float:
         weight_end = prefix.find(">")
         weight = prefix[weight_start:weight_end]
     else:
-        weight = 1.0
+        weight = 0
     try:
         weight = float(weight)
     except ValueError:
-        weight = 1.0
+        weight = 0
     return weight
 
 
@@ -21,6 +32,17 @@ def extract_sentence_cloud(prefix: str) -> str:
     if prefix.find("//") > -1:
         sentence_start = prefix.find("//") + 2
         return prefix[sentence_start:]
+
+
+def verify_selection(previous_actions: list, requested_actions: list) -> bool:
+    matches = 0
+    for previous_action in previous_actions:
+        for requested_action in requested_actions:
+            if previous_action[0] == requested_action[0] and previous_action[1] == requested_action[1]:
+                matches += 1
+    print(matches)
+    if matches >= len(requested_actions):
+        return True
 
 
 # Procedural Functions
@@ -72,11 +94,12 @@ def separate_action_components(previous_ruleset: list) -> list:
     for previous_rule in previous_ruleset:
         components = previous_rule["action"]
         previous_rule["actions_list"] = []
-        if components:
-            components = components[2:-2]
-            components = components.split(";")
-            previous_rule["actions_list"] = components
-            updated_ruleset.append(previous_rule)
+        if not components:
+            continue
+        components = components[2:-2]
+        components = components.split(";")
+        previous_rule["actions_list"] = components
+        updated_ruleset.append(previous_rule)
     return updated_ruleset
 
 
@@ -86,7 +109,7 @@ def separate_action_subcomponents(previous_ruleset: list) -> list:
         subcomponents_list = []
         for previous_action in previous_rule["actions_list"]:
             previous_actions = previous_action.split(".")
-            if previous_actions[0].find("AddProp") == -1:
+            if previous_actions[0].find("AddProp") == -1 and previous_actions[0].find("RemoveProp") == -1:
                 continue
             current_action = previous_actions[1][:-1].split("=")
             subcomponents_list.append(current_action)
@@ -95,13 +118,74 @@ def separate_action_subcomponents(previous_ruleset: list) -> list:
     return updated_ruleset
 
 
+def select_requested_actions(previous_ruleset: list, requested_actions: list) -> list:
+    updated_ruleset = []
+    for previous_rule in previous_ruleset:
+        matches = 0
+        for previous_action in previous_rule["actions_dict"]:
+            for requested_action in requested_actions:
+                if previous_action[0] == requested_action[0] and previous_action[1] == requested_action[1]:
+                    matches += 1
+                    break
+        if matches >= len(requested_actions):
+            updated_ruleset.append(previous_rule)
+    return updated_ruleset
+
+
+def reconstruct_rule(rule: dict) -> str:
+    rule_text = ""
+    if rule["sentence_cloud"]:
+        rule_text += "//" + rule["sentence_cloud"] + "\n"
+    if rule["weight"] != 0:
+        rule_text += "<" + str(rule["weight"]) + "> "
+    rule_text += rule["text"]
+    return rule_text
+
+
+def generate_output_file_name(requested_actions: list) -> str:
+    name = "rules"
+    for requested_action in requested_actions:
+        name += "_" + requested_action[0] + "_" + requested_action[1]
+    name += ".rls"
+    return name
+
+
+def write_file(rules: list, name: str, input_file: str):
+    text = ""
+    for rule in rules:
+        rule_text = reconstruct_rule(rule)
+        text += rule_text + "\n\n"
+    file_path = Path(input_file).parent / name
+    file_path.write_text(text)
+
+
+def cleanup(rules: list, name: str, input_file: str, cc: bool, x: bool):
+    input_path = Path(input_file)
+    if x:
+        input_path.write_text("#include " + name + "\n\n" + input_path.read_text())
+    if cc:
+        file_text = input_path.read_text()
+        for rule in rules:
+            rule_text = reconstruct_rule(rule)
+            file_text = file_text.replace(rule_text, "")
+            input_path.write_text(file_text)
+
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="")
-    parser.add_argument("input", metavar="input_file_path", type=str, help="Path of input rule set file for "
-                                                                           "examination.")
-    args = parser.parse_args()
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("input", metavar="input_file_path", type=str, help="Path of input rule set file for "
+                                                                               "examination.")
+    arg_parser.add_argument("--actions", required=True, nargs="*", action=KeyValue, help="hi")
+    arg_parser.add_argument("-cc", action="store_true", help="")
+    arg_parser.add_argument("-x", action="store_true", help="")
+
+    args = arg_parser.parse_args()
     txt = read_file(args.input)
     input_ruleset = enumerate_rules(txt)
     input_ruleset = separate_rule_components(input_ruleset)
     input_ruleset = separate_action_components(input_ruleset)
     input_ruleset = separate_action_subcomponents(input_ruleset)
+    output_ruleset = select_requested_actions(input_ruleset, args.actions)
+    file_name = generate_output_file_name(args.actions)
+    write_file(output_ruleset, file_name, args.input)
+    cleanup(output_ruleset, file_name, args.input, args.cc, args.x)
